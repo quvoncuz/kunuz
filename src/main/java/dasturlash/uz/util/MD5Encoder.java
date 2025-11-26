59920e0398976718a35640063ad35aa68705f908
quvoncuz <quvoncbe.uz@gmail.com>
2025-11-26 19:50:55 +0500

    MD%Encoder added

---
+++ src/main/java/dasturlash/uz/util/MD5Encoder.java
@@ -0,0 +1,22 @@
+package dasturlash.uz.util;
+
+import java.security.MessageDigest;
+
+public class MD5Encoder {
+    public static String md5(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] digest = md.digest(input.getBytes(java.nio.charset.StandardCharsets.UTF_8));
+
+            StringBuilder sb = new StringBuilder();
+            for (byte b : digest) {
+                sb.append(String.format("%02x", b));
+            }
+            return sb.toString();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
