24b0a4c5efbf08577192ce28e28c63e4ebe3f13d
quvoncuz <quvoncbe.uz@gmail.com>
2025-11-28 18:25:32 +0500

    Delete query added

--- src/main/java/dasturlash/uz/repository/ProfileRoleRepository.java
+++ src/main/java/dasturlash/uz/repository/ProfileRoleRepository.java
@@ -5,6 +5,7 @@ import dasturlash.uz.enums.Role;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,7 +14,16 @@ import java.util.List;
 public interface ProfileRoleRepository extends JpaRepository<ProfileRoleEntity, Integer> {
     List<Role> findAllByProfileId(Integer id);
 
-    @Modifying
+    @Query("select roles from ProfileRoleEntity where profileId =?1")
+    List<Role> getRoleListByProfileId(Integer profileId);
+
     @Transactional
-    void deleteByProfileId(Integer id);
+    @Modifying
+    @Query("Delete from ProfileRoleEntity where profileId =?1 and roles =?2")
+    void deleteByIdAndRoleEnum(Integer profileId, Role role);
+
+    @Transactional
+    @Modifying
+    @Query("Delete from ProfileRoleEntity where profileId =?1")
+    void deleteByProfileId(Integer profileId);
 }
