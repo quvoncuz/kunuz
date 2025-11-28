9e05e2a703f639486ecccd882962da6fc1c88c86
quvoncuz <quvoncbe.uz@gmail.com>
2025-11-28 18:27:56 +0500

    Registration DTO

---
+++ src/main/java/dasturlash/uz/dto/RegistrationDTO.java
@@ -0,0 +1,18 @@
+package dasturlash.uz.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegistrationDTO { // TODO write validation
+    @NotBlank(message = "Name required")
+    private String name;
+    @NotBlank(message = "Surname required")
+    private String surname;
+    @NotBlank(message = "Username required")
+    private String username;
+    @NotBlank(message = "password required")
+    private String password;
+}
