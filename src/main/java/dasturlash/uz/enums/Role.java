d17e27b066e9ea0ad25202ca4f1ea8b0e928b337
quvoncuz <quvoncbe.uz@gmail.com>
2025-11-28 18:21:08 +0500

    methods added to Role Enum

--- src/main/java/dasturlash/uz/enums/Role.java
+++ src/main/java/dasturlash/uz/enums/Role.java
@@ -1,8 +1,26 @@
 package dasturlash.uz.enums;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public enum Role{
-    ROLE_ADMIN,
-    ROLE_MODERATOR,
-    ROLE_PUBLISHER,
-    ROLE_USER,
+    ROLE_USER, ROLE_ADMIN, ROLE_MODERATOR, ROLE_PUBLISH;
+
+    public static List<Role> values(String[] roles) {
+        if (roles == null || roles.length == 0) {
+            return new LinkedList<>();
+        }
+        List<Role> roleList = new LinkedList<>();
+        for (String role : roles) {
+            roleList.add(Role.valueOf(role));
+        }
+        return roleList;
+    }
+
+    public static List<Role> valuesFromStr(String valuesStr) {
+        if (valuesStr == null) {
+            return new LinkedList<>();
+        }
+        return values(valuesStr.split(","));
+    }
 }
